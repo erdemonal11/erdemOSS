ecab90e5887abbf270ac6ef04dafe089b4e74a27
erdemonal11 <erdemonal11@outlook.com>
2024-08-23 14:54:11 +0300

    husky added.

--- dist/index.js
+++ dist/index.js
@@ -31119,7 +31119,9 @@ async function run() {
         (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : "Unknown error");
     }
 }
-run();
+if (!process.env.JEST_WORKER_ID) {
+    run();
+}
 
 })();
 
